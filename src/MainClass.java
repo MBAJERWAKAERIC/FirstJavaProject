9eb9d19eae26908b6d47b778701320c80105e866
Mbaje Rwaka Eric <60686069+MBAJERWAKAERIC@users.noreply.github.com>
2022-07-31 21:03:05 +0300

    Add files via upload

---
+++ src/MainClass.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author ericmbajerwaka
+ */
+public class MainClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello Netbeans 12.5");
+        
+        MainFrame myFrame = new MainFrame();
+        myFrame.init();
+    }
+    
+}
---
+++ src/MainFrame.java
@@ -0,0 +1,14 @@
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+
+public class MainFrame extends JFrame {
+    public void init() {
+        setTitle("Welcome");
+        setSize(450, 300);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+    
+}
